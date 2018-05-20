0c993e2058a57613d57657fa4c0c92a289328e85
Innocent <innocent.amadi@andela.com>
2018-05-20 16:18:06 +0100

    #157672939 remove old login page

--- src/js/components/login/actions/index.js
+++ src/js/components/login/actions/index.js
@@ -1,4 +1,19 @@
 import processRequest from '../../../utils/webAPI'
 import cookie from 'js-cookie'
 
-
+export const fakeAuth = {
+  isLoggedIn: true,
+  currentUser: {},
+  authenticate(cb) {
+    this.isLoggedIn = true;
+    this.currentUser = {
+      name: 'Kent Beck'
+    }
+    setTimeout(cb, 100)
+  },
+  signout(cb) {
+    this.isLoggedIn = false
+    this.currentUser = {}
+    setTimeout(cb, 100)
+  }
+}
