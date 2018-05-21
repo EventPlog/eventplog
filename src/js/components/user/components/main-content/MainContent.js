3af707869eaac4a14e7c7c2d57144e16ab11176f
boobae <egbunaoluebube@gmail.com>
2018-05-21 21:04:35 +0100

    user component maincontnt style update

--- src/js/components/user/components/main-content/MainContent.js
+++ src/js/components/user/components/main-content/MainContent.js
@@ -8,7 +8,8 @@ import SignupForm from '../../../login/components/signup-form'
 const StyledMainContent = styled.div`
   > .app-container {
     display: flex;
-    z-index: 1
+    z-index: 1;
+    padding-top: 150px
   }
   
   .caption {
@@ -27,6 +28,8 @@ const StyledMainContent = styled.div`
       font-weight: 300;
       font-family: "Andale Mono", AndaleMono, monospace;
     }
+
+    
     
     small {
       font-size: 1.7rem;
@@ -34,7 +37,18 @@ const StyledMainContent = styled.div`
       margin-top: 10px;
     }
   }
-  
+  h3{
+        text-align: center;
+        font-weight: bold;
+        text-transform: uppercase
+    }
+    .maincontent-div{
+      height: 450px;
+      padding-top: 150px
+    }
+  .to-get-started-message{
+      text-align:center
+  }
   .form-holder {
     background: ${colors.white};
     border-radius: 5px;
@@ -54,18 +68,20 @@ const loadLoginComponentByPath = (path) => (
 )
 const MainContent = ({ status, token, confirmed }) => (
   <StyledMainContent className="app-container">
-    <h3>Confirm your email!</h3>
+    <div className="maincontent-div">
+    <h3 id="confirm-mail">Confirm your email!</h3>
     {token && <div>
       {status == 'processing' && 'Confirming your email, please wait....'}
       {confirmed() && 'Your email is confirmed! Redirecting you back to login page ...'}
       {(status == 'failed') && 'You email confirmation failed. Please try again'}
     </div>}
     {!token &&
-    <div>
-      We've sent you a mail with a link to confirm your email address. &nbsp;
+    <div className="to-get-started-message">
+      We have sent you a mail with a link to confirm your email address. &nbsp;
       Please check your emal to get started.
     </div>
     }
+    </div>
   </StyledMainContent>
 )
 
