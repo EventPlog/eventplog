fa3a0928486a9f95fec3e1b18a2dc9b93802f37e
Innocent <innocent.amadi@andela.com>
2018-06-02 01:20:07 +0100

    user heroku ssl-redirect for http redirects

--- scripts/server.js
+++ scripts/server.js
@@ -3,19 +3,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const cors = require('cors')
+const sslRedirect = require('heroku-ssl-redirect');
 
 const port = process.env.PORT || 9000
 
+// enable ssl redirect
+app.use(sslRedirect());
+
 app.use(cors())
 app.use(express.static(path.join(__dirname, '..', 'build')));
-
-/* Redirect http to https */
-app.get('*', function(req,res,next) {
-  if(req.headers['x-forwarded-proto'] != 'https' && process.env.NODE_ENV === 'production')
-    res.redirect('https://'+req.hostname+req.url)
-  else
-    next() /* Continue to other routes if we're not redirecting */
-});
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '..','build', 'index.html'));
