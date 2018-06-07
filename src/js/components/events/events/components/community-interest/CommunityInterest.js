28641234e031ebc541efe7bc87767bc0c6a5f90a
boobae <egbunaoluebube@gmail.com>
2018-06-08 00:43:26 +0100

    rename css classes

--- src/js/components/events/events/components/community-interest/CommunityInterest.js
+++ src/js/components/events/events/components/community-interest/CommunityInterest.js
@@ -7,38 +7,31 @@ import {Link} from 'react-router-dom'
 const StyledCommunityInterest = styled.div`
 
 
-.communityinterestdiv{
+.community-interest-div{
     background: #f8f8f8;
     padding: 10px 20px;
 }
-.communityinterestchild{
-  
-}
-.communityinterestheader{
+
+.community-interest-header{
     margin: 10px 10px;
     padding-top: 35px;
     padding-left: 15px;
 }
-.communityinterestcard{
+.community-interest-card{
     background: ${colors.white};
     margin: 30px 20px; 
 }
-.communityinterestimage img{
-    
-    
-}
+
 img{
     width:100%;
     height: 100px;
 }
-.communityinterestinfo{
+.community-interest-info{
  display: block;
  margin: 5px 22px;
 }
-.communityinterestbutton{
 
-}
-.communityinterestinfoheader{
+.community-interest-info-header{
     color: ${colors.primary}
     font-size: 1.5rem;
 }
@@ -48,20 +41,20 @@ img{
 const CommunityInterest = () =>{
     return(
         <StyledCommunityInterest>
-        <div className="communityinterestdiv">
-            <div className="communityinterestchild">
-            <div className="communityinterestheader">
+        <div className="community-interest-div">
+            <div className="community-interest-child">
+            <div className="community-interest-header">
                 <p>Because of your interest<br/> in Developer Circles</p>
             </div>
-            <div className="communityinterestcard">
-                <div className="communityinterestimage">
+            <div className="community-interest-card">
+                <div className="community-interest-image">
                 <img src="image"/>
                 </div>
-                <div className="communityinterestinfo">
-                    <div className="communityinterestinfoheader">Title</div>
-                    <div className="communityinterestcategory">Technology Community</div>
-                    <div className="communityinterestcommunitysize">220 members</div>
-                    <div className="communityinterestbutton"><Button>Join</Button></div>
+                <div className="community-interest-info">
+                    <div className="community-interest-info-header">Title</div>
+                    <div className="community-interest-category">Technology Community</div>
+                    <div className="community-interest-community-size">220 members</div>
+                    <div className="community-interest-button"><Button>Join</Button></div>
                 </div>
             </div>
             </div>
