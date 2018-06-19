1b5f23463c8d1e1dfc78816862e219467ec3e7c3
boobae <egbunaoluebube@gmail.com>
2018-06-19 03:55:21 +0100

    add responsiveness to terms and conditions

--- src/js/components/terms-and-conditions/TermsAndConditions.js
+++ src/js/components/terms-and-conditions/TermsAndConditions.js
@@ -2,10 +2,16 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import styled from 'styled-components'
 import TermsAndConditionsPage from './TermsAndConditionsPage'
+import {media} from '../../styles/mixins'
 
 const StyledTermsAndConditions = styled.div`
 display: flex;
 margin: 50px 100px;
+  ${
+    media.phone`
+      margin: 10px 20px;
+    `
+  }
 `
 
 const TermsAndConditions = () => (
