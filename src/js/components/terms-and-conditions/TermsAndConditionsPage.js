9e8509faa5ad4b3fdf99625d7f509168380af1e8
boobae <egbunaoluebube@gmail.com>
2018-06-19 03:26:24 +0100

    update terms and conditions

--- src/js/components/terms-and-conditions/TermsAndConditionsPage.js
+++ src/js/components/terms-and-conditions/TermsAndConditionsPage.js
@@ -17,13 +17,12 @@ const TermsAndConditionsPage =()=>{
         <heading><h5 className="title"> Terms and Conditions</h5></heading><br/>
         
         <text className="content">A Terms and Conditions agreement is the agreement that includes the terms, the rules and the guidelines of acceptable behavior, plus other useful sections, to which users must agree in order to use or access your website and mobile app.</text>
-          <subtitle><h6>How may I help</h6></subtitle>
+          <subtitle><h6>Intellectual Property Rights</h6></subtitle>
           <text className="content">A Terms and Conditions agreement is the agreement that includes the terms, the rules and the guidelines of acceptable behavior, plus other useful sections, to which users must agree in order to use or access your website and mobile app.</text>
-          <subtitle><h6>How may I help</h6></subtitle>
+          <subtitle><h6>Restrictions</h6></subtitle>
           <text className="content">A Terms and Conditions agreement is the agreement that includes the terms, the rules and the guidelines of acceptable behavior, plus other useful sections, to which users must agree in order to use or access your website and mobile app.</text>
-          <subtitle><h6>How may I help</h6></subtitle>
+          <subtitle><h6>Limitation of liability</h6></subtitle>
           <text className="content">A Terms and Conditions agreement is the agreement that includes the terms, the rules and the guidelines of acceptable behavior, plus other useful sections, to which users must agree in order to use or access your website and mobile app.</text>
-          
 
     </div>
     </StyledTermsAndConditionsPage>
