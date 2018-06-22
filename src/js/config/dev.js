464a36dfee8de45f983df64989e2312a1c49793a
boobae <egbunaoluebube@gmail.com>
2018-06-22 16:32:08 +0100

    revert to backend heroku app

--- src/js/config/dev.js
+++ src/js/config/dev.js
@@ -1,4 +1,4 @@
 export default {
-  // host: process.env.EVENTPLOG_API || 'http://techmeetupsng-backend.herokuapp.com'
-  host: process.env.EVENTPLOG_API || 'http://localhost:8000'
+  host: process.env.EVENTPLOG_API || 'http://techmeetupsng-backend.herokuapp.com'
+  //host: process.env.EVENTPLOG_API || 'http://localhost:8000'
 }
