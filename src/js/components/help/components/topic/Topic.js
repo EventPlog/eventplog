654adefb3c6481732139dc74ae538e6de4778eee
boobae <egbunaoluebube@gmail.com>
2018-06-26 19:12:38 +0100

    make topic  component

---
+++ src/js/components/help/components/topic/Topic.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+import {media } from '../../../../styles/mixins'
+
+const StyledTopic =styled.div`
+.topic{
+  text-align: center;
+  font-weight:bold;
+}
+`
+
+const Topic = (props) =>{
+  return(
+    <StyledTopic>
+      <div className="topic">{props.children}</div>
+    </StyledTopic>
+  )
+}
+
+export default Topic
