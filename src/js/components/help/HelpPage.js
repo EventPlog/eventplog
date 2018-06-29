b8bbf917dc0bf4f9722cc1549ad8aec9c5bb324c
boobae <egbunaoluebube@gmail.com>
2018-06-29 15:17:32 +0100

    add getting started page routes and communities routes using  webpack Chunck

--- src/js/components/help/HelpPage.js
+++ src/js/components/help/HelpPage.js
@@ -1,24 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
 import {Switch, Route,Link} from 'react-router-dom'
-import HelpDescription from './components/help-description'
-import HelpTopics from './components/help-topics'
 import createLoader from '../../components/shared/loading/createLoadable'
 
 const StyledHelpPage =styled.div`
 `
 const GettingStarted= createLoader(() => import('./components/pages/getting-started' /* webpackChunckName : "GettingStarted" */))
+const CommunitiesHelp= createLoader(() => import('./components/pages/communities' /*webpackChunkName : "CommunitiesHelp" */))
+//const Events= createLoader(() => import('./components/pages/events' /*webpackChunkName : "EventsHelp" */))
 
 const HelpPage =()=>{
   return(
-  <StyledHelpPage>
-    <Switch>
-      <Route path="/" component={GettingStarted} />
-      <Route path="/help/getting-started" component={GettingStarted} />
-      {/* {<Route exact path="/help/communities" component={Communities} />*/}
-    
-    </Switch>
-  </StyledHelpPage>
+    <StyledHelpPage>
+      <Switch>
+        <Route exact path="/help" component={GettingStarted} />
+        <Route exact path="/help/getting-started" component={GettingStarted} />
+        <Route path="/help/communities" component={CommunitiesHelp}/> 
+      </Switch>
+    </StyledHelpPage>
   );
 }
 
