6d261fe174d03ae0e93861fb245bb41daeac156d
boobae <egbunaoluebube@gmail.com>
2018-07-01 19:06:28 +0100

    create organizers help section

---
+++ src/js/components/help/components/pages/organisers/create-a-community/CreateACommunity.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+import Sidebar from '../../../../help-topics'
+import Topic from '../../../../topic'
+import Desrciption from '../../../../help-description'
+
+const StyledCreateACommunity = styled.div`
+
+`
+
+const CreateACommunity = () =>{
+  return(
+    <Sidebar/>
+  )
+}
+
+export default CreateACommunity
