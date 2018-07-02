687f12b70a4c1c51f63ec6572ed40616c84dd5ca
boobae <egbunaoluebube@gmail.com>
2018-07-02 15:30:04 +0100

    create backstage help pages

---
+++ src/js/components/help/components/pages/backstage/scenes/event-settings/EventSettiings.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+import Sidebar from '../../../../help-topics'
+import Topic from '../../../../topic'
+import Desrciption from '../../../../help-description'
+import Description from '../../../../help-description';
+import {Link} from 'react-router-dom'
+
+const StyledEventGuests=styled.div`
+
+`
+const EventPlanning = ()=>{
+  return(
+    <StyledEventGuests>
+      <Sidebar/>
+      <Topic>Guests</Topic>
+      <Description>My guests</Description>
+    </StyledEventGuests>
+  )
+}
+
+export default EventPlanning
