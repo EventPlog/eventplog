45e75bfc05632c5b328489cb8f5115bfb94a2e9a
boobae <egbunaoluebube@gmail.com>
2018-07-03 18:30:52 +0100

    add help images

--- src/js/components/help/components/pages/getting-started/GettingStarted.js
+++ src/js/components/help/components/pages/getting-started/GettingStarted.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-//import image from '../../../../../img/help-pages/help login page desktop.jpg'
+import image from '../../../../../img/help-pages/'
 import image from '../../../../../../img/help-pages/help login page desktop.jpg'
 import { Link} from 'react-router-dom'
 import Topic from '../../topic'
