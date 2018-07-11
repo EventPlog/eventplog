c1ca90f2dd7d20345ba2ab242f3eec3006a8a0f4
Princesso <egbunaoluebube@gmail.com>
2018-07-12 00:42:30 +0100

    update events help pages

--- src/js/components/help/components/pages/events/scenes/upcoming-and-past-events/AllEvents.js
+++ src/js/components/help/components/pages/events/scenes/upcoming-and-past-events/AllEvents.js
@@ -1,31 +1,17 @@
 import React from 'react'
-import styled from 'styled-components'
 import {Link} from 'react-router-dom'
-import Sidebar from '../../../../help-topics'
-import Topic from '../../../../topic'
-import Description from '../../../../help-description'
+import Topic from 'js/components/help/components/topic'
+import Description from 'js/components/help/components/help-description'
 
-const StyledDiscoverEvents=styled.div`
-img{
-  width:700px;
-}
-`
-
-const AllEvents = ()=>{
+const AllEvents = () => {
   return(
-    <StyledDiscoverEvents>
-      <div className="app-container">
-        <div ><Sidebar/></div> 
-
-        <div className="help-main-content"> 
-          <Topic>My Events</Topic>
-          <Description>
-            To find all events you indicated interest in, whether past or upcoming, hit the MyEvents button
-            on ... <br/><br/>
-          </Description> 
-        </div>
-      </div>
-    </StyledDiscoverEvents>
+    <div> 
+      <Topic>My Events</Topic>
+      <Description>
+        To find all events you indicated interest in, whether past or upcoming, hit the MyEvents button
+        on ... <br/><br/>
+      </Description> 
+    </div>
   )
 }
 
