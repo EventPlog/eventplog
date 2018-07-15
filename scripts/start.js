def037640abd68af974d73f2ec083eeaddfa3abd
Innocent <innocent.amadi@andela.com>
2018-07-15 11:13:18 +0100

    fix environment swap issues

--- scripts/start.js
+++ scripts/start.js
@@ -1,8 +1,15 @@
 'use strict';
 
+const dotenv = require('dotenv')
+
+const env = dotenv.config().parsed
+
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'development';
-process.env.NODE_ENV = 'development';
+process.env.BABEL_ENV = process.env.BABEL_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_PATH = 'src';
+
+console.log(`Building for node environment: ${process.env.NODE_ENV}`)
 
 require('newrelic')
 // Makes the script crash on unhandled rejections instead of silently
@@ -87,7 +94,7 @@ choosePort(HOST, DEFAULT_PORT)
         return console.log(err);
       }
       if (isInteractive) {
-        clearConsole();
+        // clearConsole();
       }
       console.log(chalk.cyan('Starting the development server...\n'));
       openBrowser(urls.localUrlForBrowser);
