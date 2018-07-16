5d6748b2b448b570a4cc393c5a6e573e2c00c7a3
Princesso <egbunaoluebube@gmail.com>
2018-07-16 22:04:35 +0100

    add usability to events pages

--- src/js/components/help/components/pages/events/scenes/discover-new-events/DiscoverNewEvents.js
+++ src/js/components/help/components/pages/events/scenes/discover-new-events/DiscoverNewEvents.js
@@ -8,10 +8,10 @@ const DiscoverEvents = () => {
     <div className="help-main-content"> 
       <Topic>Discover Events</Topic>
       <Description>
+        When you search for events in a field, you want to find a list of events all related to one field 
+        say technology, so you can indicate interest in those that appeal to you.
         Use the search bar to look for events you may be interested in, if our event suggestions do not capture
         events you would love to be a part of.
-        <br/><br/> Announcements can be found on all <Link to="">Event</Link>
-        and <Link to="">Community</Link> pages. <br/><br/> 
       </Description> 
     </div>
   )
