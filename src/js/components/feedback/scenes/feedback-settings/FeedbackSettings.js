62a072c9a765e1525d70963a667bb3457a383d24
Princesso <egbunaoluebube@gmail.com>
2018-07-20 12:18:25 +0100

    update element and attribute values for feedback form

--- src/js/components/feedback/scenes/feedback-settings/FeedbackSettings.js
+++ src/js/components/feedback/scenes/feedback-settings/FeedbackSettings.js
@@ -32,13 +32,19 @@ const StyleFeedbackSettings = styled.div`
 `
 
 const FeedbackSettings = ({
-  event = {},
+  feedback = {},
   success,
   loading,
   handleChange,
-  handleSubmit
+  handleSubmit,
+  handleChangeForRadio,
 }) => {
-  const { user = {}, feedback_url, value } = event;
+  const { 
+    feedback_url, 
+    active_feedback_form, 
+    feedback_message, 
+    post_feedback_message 
+  } = feedback;
   return (
     <StyleFeedbackSettings>
       <ContentPanel title="Feedback settings">
@@ -49,14 +55,14 @@ const FeedbackSettings = ({
             content="You've successfully checked into this event!"
           />
 
-          {feedback_url && <FeedbackUrl url={feedback_url} />}
           <Form.Group grouped>
             <label>Active feedback form</label>
             <Form.Field>
               <Radio
                 label='Use the default form'
+                name="active_feedback_form"
                 value='default'
-                checked={value === 'default'}
+                checked={active_feedback_form === 'default'}
                 onChange={handleChange}
               />
             </Form.Field>
@@ -64,13 +70,14 @@ const FeedbackSettings = ({
             <Form.Field>
               <Radio
                 label='Use a different form'
-                value='default'
-                checked={value === 'custom'}
+                name="active_feedback_form"
+                value='custom'
+                checked={active_feedback_form === 'custom'}
                 onChange={handleChange}
               />
               <div className="radio-input">
                 <Input name="feedback_url"
-                       value={``}
+                       value={feedback_url}
                        placeholder='url for feedback form' onChange={handleChange}/>
               </div>
             </Form.Field>
@@ -79,26 +86,42 @@ const FeedbackSettings = ({
 
           <Form.Group grouped>
             <label>Send Prompt for feedback</label>
-            <Form.Field label='30 minutes before end of the event' control={Checkbox} />
-            <Form.Field label='At the end of the event' control={Checkbox} />
-            <Form.Field label='One day after the event' control={Checkbox} />
-            <Form.Field label='Three days after the event' control={Checkbox} />
+
+            <Form.Field label='30 minutes before end of the event' 
+                        name='half_an_hour_prompt' 
+                        onChange={handleChange} 
+                        control={Checkbox} />
+
+            <Form.Field label='At the end of the event' 
+                        name='end_of_day_prompt' 
+                        onChange={handleChange} 
+                        control={Checkbox} />
+
+            <Form.Field label='One day after the event' 
+                        name='one_day_later_prompt' 
+                        onChange={handleChange} 
+                        control={Checkbox} />
+
+            <Form.Field label='Three days after the event' 
+                        name='three_days_later_prompt' 
+                        onChange={handleChange} 
+                        control={Checkbox} />
           </Form.Group>
 
           <Form.Group>
             <Form.Field>
               <label>Custom message to appeal to guests to give feedback</label>
               <TextArea name="feedback_message"
-                        value={``}
+                        value={feedback_message}
                         placeholder='Could you help us improve by giving feedback? Thanks!' onChange={handleChange}/>
             </Form.Field>
           </Form.Group>
 
           <Form.Group>
             <Form.Field>
-              <label>Custom message to appeal to guests to give feedback</label>
+              <label>Custom message to thank guests for giving feedback</label>
               <TextArea name="post_feedback_message"
-                        value={``}
+                        value={post_feedback_message}
                         placeholder="Thank you for giving feedback!" onChange={handleChange}/>
             </Form.Field>
           </Form.Group>
