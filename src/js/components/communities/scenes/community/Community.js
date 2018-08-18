ff675c9b1735eea8d9e381bfaea464668f388b51
Innocent <innocent.amadi@andela.com>
2018-08-18 08:27:12 +0100

    update community header

--- src/js/components/communities/scenes/community/Community.js
+++ src/js/components/communities/scenes/community/Community.js
@@ -37,7 +37,7 @@ const Main = ({activeLink, ...props}) => {
   }
   return (
     <StyledMain activeLink={activeLink}>
-      {props.community && props.community.id && <CommunityHeader {...props} />}
+      <CommunityHeader {...props} />
       <Switch>
         <PrivateRoute exact path="/communities/:id" render={(routerProps) => <CommunityMainContent {...props} />}/>
         <PrivateRoute path="/communities/:id/edit" render={() => <UpdateCommunity {...props} />} />
