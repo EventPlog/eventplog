1391a16e0a67bc1ce0be3c395636fbf31d1897e2
Princesso <egbunaoluebube@gmail.com>
2018-09-13 18:26:27 +0100

    how to assign task

---
+++ src/js/components/help/components/pages/organisers/scenes/assign-task/AssignTask.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Topic from 'js/components/help/components/topic'
+import Description from 'js/components/help/components/help-description'
+import {Link} from 'react-router-dom'
+import createcommunityimage from 'img/help-pages/create-a-community.png'
+import createcommunity from 'img/help-pages/createcommunitystep2.png'
+import HelpImage from 'js/components/help/components/image'
+
+const AddCoOrganizers = () =>{
+  return(
+    <div>
+      <Topic>
+        How to Add Co-organizers
+      </Topic>
+      <Description>
+
+      </Description>
+    </div>
+  );
+}
+
+export default AddCoOrganizers;
