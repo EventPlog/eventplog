0f7ec68c69f9776f164d48d597433ffd4095598e
Princesso <egbunaoluebube@gmail.com>
2018-09-14 09:09:56 +0100

    update add organizers

--- src/js/components/help/components/pages/organisers/scenes/add-co-organizers/AddCoOrganizers.js
+++ src/js/components/help/components/pages/organisers/scenes/add-co-organizers/AddCoOrganizers.js
@@ -2,9 +2,8 @@ import React from 'react'
 import Topic from 'js/components/help/components/topic'
 import Description from 'js/components/help/components/help-description'
 import {Link} from 'react-router-dom'
-import createcommunityimage from 'img/help-pages/create-a-community.png'
-import createcommunity from 'img/help-pages/createcommunitystep2.png'
 import HelpImage from 'js/components/help/components/image'
+import Image from 'js/components/help/components/videos'
 
 const AddCoOrganizers = () =>{
   return(
@@ -13,7 +12,10 @@ const AddCoOrganizers = () =>{
         How to Add Co-organizers
       </Topic>
       <Description>
-
+        To add co-organizers, click on the backstage button and on settings.
+        Add organizer by clicking on organizer. Enter the e-mail addresses of
+        any co-organizer you want to add to your event to invite them by clicking the 
+        invite button.
       </Description>
     </div>
   );
