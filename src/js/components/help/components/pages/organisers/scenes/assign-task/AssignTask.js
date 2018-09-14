2af4b8f871fbb16e6f445acba34d910fba2b23ac
Princesso <egbunaoluebube@gmail.com>
2018-09-14 09:10:41 +0100

    update how to assign task

--- src/js/components/help/components/pages/organisers/scenes/assign-task/AssignTask.js
+++ src/js/components/help/components/pages/organisers/scenes/assign-task/AssignTask.js
@@ -2,21 +2,22 @@ import React from 'react'
 import Topic from 'js/components/help/components/topic'
 import Description from 'js/components/help/components/help-description'
 import {Link} from 'react-router-dom'
-import createcommunityimage from 'img/help-pages/create-a-community.png'
-import createcommunity from 'img/help-pages/createcommunitystep2.png'
 import HelpImage from 'js/components/help/components/image'
 
-const AddCoOrganizers = () =>{
+const AssignTask = () =>{
   return(
     <div>
       <Topic>
-        How to Add Co-organizers
+        How to Add Assign Tasks to Co-organizers
       </Topic>
       <Description>
-
+        Sharing tasks can make planning less burdensom. To assign tasks to co-organizers,
+        click on the backstage button then click on planning, go to the all tasks section and 
+        scroll to the bottom to add tasks. You can also add task by using the add task pane on the 
+        planning section.
       </Description>
     </div>
   );
 }
 
-export default AddCoOrganizers;
+export default AssignTask;
