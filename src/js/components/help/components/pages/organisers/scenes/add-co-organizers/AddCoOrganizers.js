036f01da7d2d345efc895d7f12109b4e25ee0555
Princesso <egbunaoluebube@gmail.com>
2018-09-18 18:50:14 +0100

    how to add co-organizers

--- src/js/components/help/components/pages/organisers/scenes/add-co-organizers/AddCoOrganizers.js
+++ src/js/components/help/components/pages/organisers/scenes/add-co-organizers/AddCoOrganizers.js
@@ -3,19 +3,40 @@ import Topic from 'js/components/help/components/topic'
 import Description from 'js/components/help/components/help-description'
 import {Link} from 'react-router-dom'
 import HelpImage from 'js/components/help/components/image'
-import Image from 'js/components/help/components/videos'
+import eventpage from 'img/blog/beforebackstage.PNG'
+import backstage from 'img/blog/backstage.png'
+import addorganizer from 'img/blog/addorganizer.png'
+import Video from 'js/components/help/components/videos'
 
-const AddCoOrganizers = () =>{
+const AddCoOrganizers = () => {
   return(
     <div>
       <Topic>
         How to Add Co-organizers
       </Topic>
       <Description>
-        To add co-organizers, click on the backstage button and on settings.
-        Add organizer by clicking on organizer. Enter the e-mail addresses of
-        any co-organizer you want to add to your event to invite them by clicking the 
-        invite button.
+        <p>
+          To add co-organizers, go to your event page. Yes! the event you just created
+          and click on the backstage button found on the top right hand corner of your event page.
+        </p>  
+        <HelpImage src={eventpage} alt="event page" />
+        <p>
+          The backstage of your event looks not so different from this. The differences lie in the information
+          you have entered while editing your event.
+        </p>
+        <HelpImage src={backstage} alt="backstage" />
+        <p>
+          On the backstage, make use of the settings button to find 'add organizers' .
+        </p>
+        <HelpImage src={addorganizer} alt=" add organizer"/>
+        <p>
+          Enter the e-mail address or addresses of anyone or people you would
+          want to add as co-organizers. Their roles may also be that of volunteers. Be sure to select the role
+          accordingly. A custom message can also be sent along with the invitation to be a co-organizer.
+        </p>
+        <p>
+          Do not forget to click on the invite button when you are done filling in the details.
+        </p>     
       </Description>
     </div>
   );
