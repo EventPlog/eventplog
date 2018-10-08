1e88b187229a5df69985d6499eb9575a17b04412
princesso <egbunaoluebube@gmail.com>
2018-10-08 08:38:54 +0100

    add user-communities reducer to communities combined reducer

--- src/js/components/communities/reducers/index.js
+++ src/js/components/communities/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import community from './community-reducer';
 import communities from './communities-reducer';
 import communities_suggestions from './communities-suggestions-reducer';
+import user_communities from './user-communities-reducer'
 
 let rootCommunitiesReducer = combineReducers({
   community,
   communities,
   communities_suggestions,
+  user_communities
 })
 
 export default rootCommunitiesReducer
