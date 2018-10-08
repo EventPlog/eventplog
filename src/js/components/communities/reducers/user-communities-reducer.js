4846ce78a199cd7c39e571648209fda2fe7c04aa
princesso <egbunaoluebube@gmail.com>
2018-10-08 08:40:21 +0100

    create usercommunities reducer and correct COMMUNITIES TO COMMUNITY, REST standard for naming

--- src/js/components/communities/reducers/user-communities-reducer.js
+++ src/js/components/communities/reducers/user-communities-reducer.js
@@ -5,11 +5,11 @@ import UpdateItemCollection from 'js/reducers/helpers'
 const CommunityReducer =(state=initialState.user_communities, action) =>{
     let data
     switch(action.type){
-        case actionsTypes.USER_COMMUNITIES_INDEX_START:
+        case actionsTypes.USER_COMMUNITY_INDEX_START:
             return {...state, loading:true}
-        case actionsTypes.USER_COMMUNITIES_INDEX_COMPLETE:
+        case actionsTypes.USER_COMMUNITY_INDEX_COMPLETE:
             return {...state, ...action.payload, loading: false}
-        case actionsTypes.USER_COMMUNITIES_INDEX_FAIL:
+        case actionsTypes.USER_COMMUNITY_INDEX_FAIL:
             return{...state, loading: false , error: action.payload}
         default :
             return state
