e067c339019d502c19a1051ede0fa735d19dc3a6
princesso <egbunaoluebube@gmail.com>
2018-10-16 12:22:33 +0100

    update logged in header

--- src/js/components/header/logged-in-header/Header.js
+++ src/js/components/header/logged-in-header/Header.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import {
+  Dropdown,
+  Icon,
+  Menu,
+} from 'semantic-ui-react'
 
 // internal
 import logo from 'img/eventplog-logo-name-inverted-small-v2.png'
-import { Icon } from 'semantic-ui-react'
 import Nav from 'js/components/shared/nav'
-import grayLogo from 'img/logo-gray-full-v2.png'
+import { genUserProfileLink } from 'js/utils'
+import UserAvatar from './UserAvatar'
 
-const Header = ({ inCommunity, hideMenu, onHideMenu}) => (
+const Header = ({
+  inCommunity,
+  hideMenu,
+  onHideMenu,
+  user = {},
+}) => (
   <div className="main-header app-container">
     <div className="logo">
       <Link to="/">
@@ -17,39 +27,36 @@ const Header = ({ inCommunity, hideMenu, onHideMenu}) => (
     </div>
 
     <Nav hideOnMobile={hideMenu}>
-      {/**<Nav.Item>
-        {!inCommunity &&
-          <Button.Link to="/c/new"
-                     activeClassName="hidden">
-              <span className="hidden-lg hidden-xs">
-                <Icon name="plus" />
-                <Icon name="users" />
-              </span>
-            <span className="hidden-md">Create a community</span>
-          </Button.Link>}
-
-        {inCommunity &&
-          <Link to="/c/new"
-                     activeClassName="hidden">
-              <span className="hidden-lg hidden-xs">
-                <Icon name="plus" />
-                <Icon name="users" />
-              </span>
-            <span className="hidden-md">Create a community</span>
-          </Link>}
-      </Nav.Item>
-        **/}
-
-      {/*<Nav.Item>*/}
-        {/*<Link to="/user/profile">My Profile</Link>*/}
-      {/*</Nav.Item>*/}
-
       <Nav.Item>
         <Link to="/events/new">Create Event</Link>
       </Nav.Item>
-
       <Nav.Item>
-        <Link to="/logout">Log out</Link>
+        <Menu.Menu position='right'>
+          <Dropdown text={<UserAvatar user={user} />}
+                    pointing
+                    className='link item'>
+            <Dropdown.Menu>
+              <Dropdown.Item>
+                <Link to={genUserProfileLink(user)}>
+                  <Icon name="user circle" /> My Profile
+                </Link>
+              </Dropdown.Item>
+
+              <Dropdown.Item>
+                <Link to={`${genUserProfileLink(user)}/settings`}>
+                  <Icon name="settings" /> Settings
+                </Link>
+              </Dropdown.Item>
+
+              <Dropdown.Divider />
+              <Dropdown.Item>
+                <Link to="/logout">
+                  <Icon name="lock open" /> Log out
+                </Link>
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Menu.Menu>
       </Nav.Item>
     </Nav>
   </div>
