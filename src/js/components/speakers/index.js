a2799778a0325ee6fe2dffae1c28a4645971d3dd
Innocent Amadi <tru2cent@gmail.com>
2018-10-23 16:33:21 +0100

    Revert " add ssl redirect to server"

--- src/js/components/speakers/index.js
+++
@@ -1,5 +0,0 @@
-import Speakers from './components/MainContent'
-import SpeakersContainer from './SpeakersContainer'
-import renderComponentWithContainer from 'js/components/shared/render-props-to-component'
-
-export default renderComponentWithContainer(SpeakersContainer, Speakers)
